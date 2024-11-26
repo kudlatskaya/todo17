89546382a49fe4ceb5ce8689d945e0ec3b925799
Valery Drazdovich <v.drozdovich@intelico.su>
2024-11-26 15:10:54 +0300

    fix homepage

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import './index.css'
 import App from './app/App'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
 )
 
 // if (process.env.NODE_ENV === 'development' && module.hot) {
